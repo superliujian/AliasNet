dc2d4627f064b41a26242c2b3c9e0d1a464f2971
刘健LiuJian <liu_jian_china@qq.com>
2012-09-04 12:32:45 +0800

    stable release v1.0(add comment)

--- Net.Superliujian.AliasNet/Net.Superliujian.AliasNet/JsonClassBase.cs
+++ Net.Superliujian.AliasNet/Net.Superliujian.AliasNet/JsonClassBase.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using Newtonsoft.Json;
-
-namespace Net.Superliujian.Common.Net
+///<summary>
+///AliasNet,提供一种简单，方便，封装，统一的GET/POST方法
+///Copyright:liujian
+///Email:liu_jian_china@qq.com
+///Web:http://www.superliujian.net
+///<summary>
+namespace Net.Superliujian.AliasNet
 {
+   /// <summary>
+   /// JsonClassBase基类，所有继承自此类的子类都可以通过基类提供的方法将JSON字符串转换为类实体
+   /// </summary>
    public class JsonClassBase
     {
+        /// <summary>
+        /// 将JSON字符串转换为类实体
+        /// </summary>
+        /// <typeparam name="T">实体类</typeparam>
+        /// <param name="json">JSON字符串</param>
+        /// <returns>类实体</returns>
        public static T FromJson<T>(string json)
        {
            return (T)new JsonSerializer().Deserialize(new JsonReader(new StringReader(json)), typeof(T));
--- Net.Superliujian.AliasNet/Net.Superliujian.AliasNet/ProxyFinder.cs
+++ Net.Superliujian.AliasNet/Net.Superliujian.AliasNet/ProxyFinder.cs
@@ -1,88 +1,106 @@
-﻿/*
- * Created by SharpDevelop.
- * User: KenLew
- * Date: 2012/3/7 星期三
- * Time: 10:45
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-
+﻿
 using System;
 using System.Collections.Generic;
 using HtmlAgilityPack;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
-namespace Net.Superliujian.Common.Net
+
+///<summary>
+///AliasNet,提供一种简单，方便，封装，统一的GET/POST方法
+///Copyright:liujian
+///Email:liu_jian_china@qq.com
+///Web:http://www.superliujian.net
+///<summary>
+///
+
+namespace Net.Superliujian.AliasNet
 {
-	[Serializable]
-	public class Proxy
-	{
-		public string IP;
+    /// <summary>
+    /// 代理
+    /// </summary>
+    [Serializable]
+    public class Proxy
+    {
+        public string IP;
         public int Port = 80;
-		public string Region;
+        public string Region;
 
         public override string ToString()
         {
             return string.Format("{0}:{1}", IP, Port);
         }
-	}
-	[Serializable]
-	public class ProxyCollection:List<Proxy>
-	{
-		public static void Serialize(ProxyCollection proxy,Stream stream)
-		{
-			BinaryFormatter formatter=new BinaryFormatter();
-			formatter.Serialize(stream,proxy);
-		}
-		
-		public static ProxyCollection Deserialize(Stream stream)
-		{
-			BinaryFormatter formatter=new BinaryFormatter();
-			return (ProxyCollection)formatter.Deserialize(stream);
-		}
-	}
-	
-	/// <summary>
-	/// Description of ProxyFinder.
-	/// </summary>
-	public class ProxyFinder
-	{
-			//获取代理地址的地方，默认为无忧代理最新代理
-			public static string _51proxied_http_fast="http://www.51proxied.com/http_fast.html";
-			public static string _51proxied_http_anonymous= "http://www.51proxied.com/http_anonymous.html";
-			public static string _51proxied_http_non_anonymous ="http://www.51proxied.com/http_non_anonymous.html";
-			public static string _51proxied_socks5 = "http://www.51proxied.com/socks5.html";
+    }
+    /// <summary>
+    /// 代理集合
+    /// </summary>
+    [Serializable]
+    public class ProxyCollection : List<Proxy>
+    {
+        public static void Serialize(ProxyCollection proxy, Stream stream)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, proxy);
+        }
+
+        public static ProxyCollection Deserialize(Stream stream)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            return (ProxyCollection)formatter.Deserialize(stream);
+        }
+    }
+
+    /// <summary>
+    /// 通过ProxyFinder可以方便的从51Proxy上得到各种当前最新的可用的代理
+    /// </summary>
+    public class ProxyFinder
+    {
+        //获取代理地址的地方，默认为无忧代理最新代理
+        //无忧快速HTTP代理URL
+        public static string _51proxied_http_fast = "http://www.51proxied.com/http_fast.html";
+        //无忧匿名HTTP代理URL
+        public static string _51proxied_http_anonymous = "http://www.51proxied.com/http_anonymous.html";
+        //无忧非匿名HTTP代理URL
+        public static string _51proxied_http_non_anonymous = "http://www.51proxied.com/http_non_anonymous.html";
+        //无忧Socket5代理URL
+        public static string _51proxied_socks5 = "http://www.51proxied.com/socks5.html";
 
 
-			public static List<Proxy> _51proxied_HttpProxy(string proxyUrl)
-			{
-				return _51proxied_HttpProxy(proxyUrl, "");
-			}
-			public static List<Proxy> _51proxied_HttpProxy(string proxyUrl,string regionFilter)
-			{
-				string resultString=new GetAndPostHelper().GETString(proxyUrl);
-				HtmlAgilityPack.HtmlDocument doc=new HtmlAgilityPack.HtmlDocument();
-				doc.LoadHtml(resultString);
-				HtmlNode  divNode=doc.GetElementbyId("tb");
-				HtmlNodeCollection tdNodesCollection=divNode.SelectNodes("//div/table//td");
-				List<HtmlNode> tdNodes=new List<HtmlNode>();
-				foreach(HtmlNode node in tdNodesCollection)
-					tdNodes.Add(node);
-				List<Proxy> proxy=new List<Proxy>();
-				for(int i=0;i<tdNodes.Count/4;i++)
-				{
-					string region=tdNodes[i*4+3].InnerText.Trim();
-					if(string.IsNullOrEmpty(regionFilter) ||region==regionFilter )
-					{
-						Proxy p=new Proxy();
-						p.IP=tdNodes[i*4+1].InnerText.Trim();;
-						p.Port=int.Parse(tdNodes[i*4+2].InnerText.Trim());
-						p.Region=region;
-						proxy.Add(p);
-					}
-				}
-				return proxy;
-			}
-	}
+        public static List<Proxy> _51proxied_HttpProxy(string proxyUrl)
+        {
+            return _51proxied_HttpProxy(proxyUrl, "");
+        }
+
+        /// <summary>
+        /// 从无忧代理网站得到当前最新的代理
+        /// </summary>
+        /// <param name="proxyUrl">代理Web URL</param>
+        /// <param name="regionFilter">过滤指定的region，不过滤使用""或null</param>
+        /// <returns>代理命令</returns>
+        public static List<Proxy> _51proxied_HttpProxy(string proxyUrl, string regionFilter)
+        {
+            string resultString = new GetAndPostHelper().GETString(proxyUrl);
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(resultString);
+            HtmlNode divNode = doc.GetElementbyId("tb");
+            HtmlNodeCollection tdNodesCollection = divNode.SelectNodes("//div/table//td");
+            List<HtmlNode> tdNodes = new List<HtmlNode>();
+            foreach (HtmlNode node in tdNodesCollection)
+                tdNodes.Add(node);
+            List<Proxy> proxy = new List<Proxy>();
+            for (int i = 0; i < tdNodes.Count / 4; i++)
+            {
+                string region = tdNodes[i * 4 + 3].InnerText.Trim();
+                if (string.IsNullOrEmpty(regionFilter) || region == regionFilter)
+                {
+                    Proxy p = new Proxy();
+                    p.IP = tdNodes[i * 4 + 1].InnerText.Trim(); ;
+                    p.Port = int.Parse(tdNodes[i * 4 + 2].InnerText.Trim());
+                    p.Region = region;
+                    proxy.Add(p);
+                }
+            }
+            return proxy;
+        }
+    }
 }
